d1843d6931d7e4e1940a340511e8fe9721b2e5d6
Johnny Wu <KarasAya@gmail.com>
2017-03-27 13:35:51 +0800

    update

--- examples/simple.js
+++ examples/simple.js
@@ -16,19 +16,23 @@ document.addEventListener('readystatechange', () => {
   // init scene
   let root = new sg.Node('root');
   let n0 = new sg.Node('n0');
-  n0.lpos = vmath.vec3.new(5, 5, 5);
+  n0.lpos = vmath.vec3.new(5, 0.5, 0);
 
   let n1 = new sg.Node('n1');
-  n1.lpos = vmath.vec3.new(-5, 5, -5);
+  n1.lpos = vmath.vec3.new(2, 0, 0);
 
   root.append(n0);
-  root.append(n1);
+  n0.append(n1);
   let list = sg.utils.flat(root);
+
+  let rot = vmath.quat.create();
 
   // frame
   shell.frame(() => {
     vmath.quat.rotateY(root.lrot, root.lrot, vmath.toRadian(1));
     vmath.quat.rotateX(n0.lrot, n0.lrot, vmath.toRadian(5));
+
+    n1.setWorldRot(rot);
 
     list.forEach(node => {
       renderer.drawNode(node);
