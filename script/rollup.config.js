2775af63be7048af7d8eeafaac5dbb412f80b30c
shay_chen <641278529@qq.com>
2018-01-24 10:30:29 +0800

    Upgrade rollup

--- script/rollup.config.js
+++ script/rollup.config.js
@@ -1,40 +1,56 @@
 'use strict';
 
+const buble = require('rollup-plugin-buble');
 const fsJetpack = require('fs-jetpack');
 const pjson = require('../package.json');
 
 let banner = `
 /*
  * ${pjson.name} v${pjson.version}
- * (c) ${new Date().getFullYear()} @Johnny Wu
+ * (c) ${new Date().getFullYear()} @gamedev-js
  * Released under the MIT License.
  */
 `;
 
 let dest = './dist';
 let file = 'debug-draw';
-let moduleName = 'ddraw';
+let name = 'ddraw';
+let sourcemap = true;
+let globals = {
+  'input': 'window.Input',
+  'vmath': 'window.vmath',
+  'regl': 'window.createREGL'
+};
 
 // clear directory
 fsJetpack.dir(dest, { empty: true });
 
 module.exports = {
-  entry: './index.js',
-  targets: [
-    { dest: `${dest}/${file}.dev.js`, format: 'iife' },
-    { dest: `${dest}/${file}.js`, format: 'cjs' },
-  ],
-  moduleName,
-  banner,
+  input: './index.js',
   external: [
     'input',
     'vmath',
     'regl'
   ],
-  globals: {
-    'input': 'window.Input',
-    'vmath': 'window.vmath',
-    'regl': 'window.createREGL'
-  },
-  sourceMap: true,
+  plugins: [
+    buble(),
+  ],
+  output: [
+    {
+      file: `${dest}/${file}.dev.js`,
+      format: 'iife',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+    {
+      file: `${dest}/${file}.js`,
+      format: 'cjs',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+  ],
 };
